74f8f844ba02d1b6e55bcf130ebb42dcf036e9a9
Gabriel <gabrieldos55570000@gmail.com>
2023-03-08 17:22:09 -0300

    colocando arquivo de produtos em formato json

--- src/data/products-json.ts
+++ src/data/products-json.ts
@@ -1,60 +1,60 @@
 const data = {
     products: [
       {
-        id: "1",
-        name: "Camiseta Feminina",
-        price: "49.99",
-        description: "Camiseta de algodão com estampa floral",
-        idUser: "1212",
-        tags: ["Roupas", "Feminino", "Verão"]
+        "id": "1",
+        "name": "Camiseta Feminina",
+        "price": "49.99",
+        "description": "Camiseta de algodão com estampa floral",
+        "idUser": "1212",
+        "tags": ["Roupas", "Feminino", "Verão"]
       },
       {
-        id: "2",
-        name: "Tênis Esportivo",
-        price: "129.90",
-        description: "Tênis para corrida com amortecimento de ar",
-        idUser: "1212",
-        tags: ["Calçados", "Esportivo", "Corrida"]
+        "id": "2",
+        "name": "Tênis Esportivo",
+        "price": "129.90",
+        "description": "Tênis para corrida com amortecimento de ar",
+        "idUser": "1212",
+        "tags": ["Calçados", "Esportivo", "Corrida"]
       },
       {
-        id: "3",
-        name: "Smartphone Android",
-        price: "899.00",
-        description: "Smartphone com tela de 6 polegadas e câmera de 64MP",
-        idUser: "1212",
-        tags: ["Tecnologia", "Android", "Celular"]
+        "id": "3",
+        "name": "Smartphone Android",
+        "price": "899.00",
+        "description": "Smartphone com tela de 6 polegadas e câmera de 64MP",
+        "idUser": "1212",
+        "tags": ["Tecnologia", "Android", "Celular"]
       },
       {
-        id: "4",
-        name: "Camisa Social Masculina",
-        price: "89.99",
-        description: "Camisa social de manga longa com tecido respirável",
-        idUser: "1212",
-        tags: ["Roupas", "Masculino", "Social"]
+        "id": "4",
+        "name": "Camisa Social Masculina",
+        "price": "89.99",
+        "description": "Camisa social de manga longa com tecido respirável",
+        "idUser": "1212",
+        "tags": ["Roupas", "Masculino", "Social"]
       },
       {
-        id: "5",
-        name: "Óculos de Sol",
-        price: "69.90",
-        description: "Óculos de sol com armação de acetato e lentes polarizadas",
-        idUser: "1212",
-        tags: ["Acessórios", "Óculos", "Sol"]
+        "id": "5",
+        "name": "Óculos de Sol",
+        "price": "69.90",
+        "description": "Óculos de sol com armação de acetato e lentes polarizadas",
+        "idUser": "1212",
+        "tags": ["Acessórios", "Óculos", "Sol"]
       },
       {
-        id: "6",
-        name: "Caixa de Som Bluetooth",
-        price: "149.00",
-        description: "Caixa de som com conectividade Bluetooth e bateria recarregável",
-        idUser: "1212",
-        tags: ["Tecnologia", "Áudio", "Bluetooth"]
+        "id": "6",
+        "name": "Caixa de Som Bluetooth",
+        "price": "149.00",
+        "description": "Caixa de som com conectividade Bluetooth e bateria recarregável",
+        "idUser": "1212",
+        "tags": ["Tecnologia", "Áudio", "Bluetooth"]
       },
       {
-        id: "7",
-        name: "Shorts Masculino",
-        price: "59.99",
-        description: "Shorts masculino com estampa tropical",
-        idUser: "1212",
-        tags: ["Roupas", "Masculino", "Tropical"]
+        "id": "7",
+        "name": "Shorts Masculino",
+        "price": "59.99",
+        "description": "Shorts masculino com estampa tropical",
+        'idUser': "1212",
+        'tags': ["Roupas", "Masculino", "Tropical"]
       }
     ]
   };
